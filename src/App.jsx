f3e6ab090707f000afacceae2a00b6a6073bfa31
hardiknasit25 <hardiknasit670@gmail.com>
2024-07-04 18:03:14 +0530

    UseEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import UseEffect from "./components/UseEffect/UseEffect";
 import UseState from "./components/Usestate/UseState";
 
 export default function App() {
   return (
     <div className="h-full w-fullp-5">
       <UseState/>
+      <UseEffect/>
     </div>
   )
 }
---
+++ src/components/UseEffect/UseEffect.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function UseEffect() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count => count + 1)
+    }, 2000)
+  }, [count])
+
+  return (
+    <div className="w-full p-5 flex justify-center items-center">
+
+      <div className="w-[30%] p-10 flex flex-col justify-center items-center gap-5 bg-[#DAD3BE] rounded-lg shadow-lg">
+        <h1 className="font-bold text-3xl text-[#254336]">UseEffect Hook Example</h1>
+        <span className="text-[#6B8A7A] font-medium text-2xl">Count is <span className="text-[#254336]">{count} </span></span>
+      </div>
+    </div>
+  )
+}
+
+export default UseEffect
